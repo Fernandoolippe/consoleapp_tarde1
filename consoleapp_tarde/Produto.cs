5e980c000a3c9f57e2de1c0906d9cc4a0f6bbaa2
FERNANDO SANTOS <fernando.santos172@fatec.sp.gov.br>
2023-03-08 14:22:38 -0300

    Criei os atributos e o método construtor

--- consoleapp_tarde/Produto.cs
+++ consoleapp_tarde/Produto.cs
@@ -8,5 +8,15 @@ namespace consoleapp_tarde
 {
     internal class Produto
     {
+        private int Id;
+        private string Descrição;
+        private decimal Valor;
+
+        public Produto(int id, string descricao, decimal valor)
+        {
+            Id = id;
+            Descrição = descricao;
+            Valor = valor;
+        }
     }
 }
