7447b6bdae217c97bb4ac4d759a355e5b88089cf
FERNANDO SANTOS <fernando.santos172@fatec.sp.gov.br>
2023-03-08 15:38:59 -0300

    Crieia Classe cliente e o construtor

---
+++ consoleapp_tarde/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleapp_tarde
+{
+    internal class Cliente
+    {
+        public Cliente(int id, string nome, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+        }
+    }
+}
